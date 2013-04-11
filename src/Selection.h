a453b58a334da5989d44a73b8c40711ba79387ca
Sam <sennis@iastate.edu>
2013-04-11 13:47:28 -0500

    Added missing header file

---
+++ src/Selection.h
@@ -0,0 +1,19 @@
+#ifndef _H_SELECTION_RIO_
+#define _H_SELECTION_RIO_
+
+class Selection
+{
+public:
+	void mouseHandler(int event, int x, int y, int flags, void*param);
+
+
+
+
+};
+
+
+
+
+
+
+#endif // _H_SELECTION_RIO_
